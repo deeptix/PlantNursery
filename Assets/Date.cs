4edaaf799ff3885bf5f22455138d5b76f72dd528
Olivia Xu <okxu@parktudor.org>
2022-03-27 23:21:35 -0400

    Initial version of calendar (need to add further functionality in script)

---
+++ Assets/Date.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Date : MonoBehaviour
+{
+    // create variable for date
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set initial button press
+        // assign date var to pressed button (e.g. first day of month)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // change variable to other date (e.g. button.pressed)
+    }
+}

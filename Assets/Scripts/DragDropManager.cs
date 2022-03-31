3fdc22ea4b5c5db002a48e4f445e3494791b7689
nikitabasu <nbasu@andrew.cmu.edu>
2022-03-31 12:40:50 -0400

    implemented dropping (snapping to position) on GO's with dropzone tag

--- Assets/Scripts/DragDropManager.cs
+++ Assets/Scripts/DragDropManager.cs
@@ -8,22 +8,41 @@ public class DragDropManager : MonoBehaviour, IPointerDownHandler, IBeginDragHan
     [SerializeField] private Canvas canvas;
 
     private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private int SIZE_ADJ_COLLIDER = 3;
+    private int OFFSET_ADJ_COLLIDER = 1;
 
     private void Awake() {
         rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void adjustDropZoneBoxColliders(int sizeAdj, int offsetAdj) {
+        GameObject[] dropZones = GameObject.FindGameObjectsWithTag("DropZone");
+
+        foreach (GameObject dropZone in dropZones) {
+            Debug.Log("Adjusting drop zone!");
+            BoxCollider2D col = dropZone.GetComponent<BoxCollider2D>();
+            col.size = new Vector2(col.size.x, col.size.y + sizeAdj);
+            col.offset = new Vector2(col.offset.x, col.offset.y + offsetAdj);
+        }
     }
 
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("OnBeginDrag");
+        canvasGroup.blocksRaycasts = false;
+        adjustDropZoneBoxColliders(SIZE_ADJ_COLLIDER, OFFSET_ADJ_COLLIDER);
     }
 
     public void OnDrag(PointerEventData eventData) {
-        Debug.Log("OnDrag");
+        // Debug.Log("OnDrag");
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData) {
         Debug.Log("OnEndDrag");
+        canvasGroup.blocksRaycasts = true;
+        adjustDropZoneBoxColliders(-1 * SIZE_ADJ_COLLIDER, -1 * OFFSET_ADJ_COLLIDER);
     }
 
     public void OnPointerDown(PointerEventData eventData) {

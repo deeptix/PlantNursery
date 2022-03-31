dbf5e5de0f09b793edb0044473188356d8b8370c
nikitabasu <nbasu@andrew.cmu.edu>
2022-03-31 11:18:46 -0400

    implemented dragging of plant

--- Assets/Scripts/DragDropManager.cs
+++ Assets/Scripts/DragDropManager.cs
@@ -5,12 +5,21 @@ using UnityEngine.EventSystems;
 
 public class DragDropManager : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform rectTransform;
+
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("OnBeginDrag");
     }
 
     public void OnDrag(PointerEventData eventData) {
         Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData) {

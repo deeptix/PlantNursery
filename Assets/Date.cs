d1c44b039577a425d87175d5360e28ff94110034
Olivia Xu <okxu@parktudor.org>
2022-04-06 18:46:09 -0400

    Changes to calendar

--- Assets/Date.cs
+++ Assets/Date.cs
@@ -5,6 +5,12 @@ using UnityEngine.UI;
 
 public class Date : MonoBehaviour
 {
+    public Button nextButton;
+    public Button prevButton;
+    public Text monthText;
+    int currMonth = 0;
+    string[] months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
     // create variable for date
 
     // Start is called before the first frame update
@@ -17,6 +23,18 @@ public class Date : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        nextButton.onClick.AddListener(GetNextMonth);
+
         // change variable to other date (e.g. button.pressed)
+    }
+
+    void GetNextMonth() {
+        monthText.text = months[currMonth];
+    }
+
+    void GetPrevMonth() {
+        if (currMonth == 0) {
+            currMonth = currMonth
+        }
     }
 }

b9a8c223b9e51b253a17ec4c500746efb24878e2
nikitabasu <nbasu@andrew.cmu.edu>
2022-04-22 04:44:39 -0400

    New and improved drag and drop - dropzones form a layer

---
+++ Assets/Scripts/DropManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropManager : MonoBehaviour
+{
+    public GameObject plantHolding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventBus.AddListener(EventTypes.DraggingPlant, new CallBack<bool, GameObject>(toggleCollider));
+    }
+
+    void toggleCollider(bool isDragging, GameObject plantDropped) 
+    {
+        if (!isDragging) {
+            // if not holding a plant or was holding the plant that's being dragged, check if drop zone is colliding with the plant
+            if (plantHolding == null || (plantHolding != null && plantHolding == plantDropped)) {
+                if (gameObject.GetComponent<BoxCollider2D>().IsTouching(plantDropped.GetComponent<BoxCollider2D>())) {
+                    // collided! update plant holding accordingly
+                    plantHolding = plantDropped;
+                } else {
+                    // no collision --> set plant holding to null
+                    plantHolding = null;
+                }
+            }
+        }
+    }
+}

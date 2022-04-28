816b65ee24a4ef9c7cf5f04e14f54a94cb7f0987
Angela Zhang <angelaz1@andrew.cmu.edu>
2022-04-28 16:06:19 -0400

    Basic SFX/BGM in

---
+++ Assets/Scripts/Tools/Fertilizer.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fertilizer : MonoBehaviour
+{
+    public float rotationAngle = 40;
+    public ParticleSystem fallingFertilizer;
+    private int FERTILIZING_RATE = 5;
+
+    BoxCollider2D fertilizerCollider;
+    Rigidbody2D rb;
+
+    PlantStateManager lastPlant;
+    SoundManager soundManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        fertilizerCollider = this.gameObject.GetComponent<BoxCollider2D>();
+
+        soundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        transform.position = mousePosition;
+        Quaternion originalRotation = fallingFertilizer.transform.rotation;
+
+        // TODO: check if falling water is colliding with plant
+        // nikita: i used fallingWater.transform.position but I don't think this is accurate
+        LayerMask mask = LayerMask.GetMask("Plants");
+        RaycastHit2D hit = Physics2D.Raycast(fallingFertilizer.transform.position, Vector2.zero, Mathf.Infinity, mask);
+
+        if (hit.collider != null)
+        {
+            PlantStateManager plant = hit.transform.gameObject.GetComponent<PlantStateManager>();
+            // plant.waterPlant(Time.deltaTime / FERTILIZING_RATE);
+            // if (plant != lastPlant) EventBus.Broadcast<PlantStateManager>(EventTypes.WateredPlant, plant);
+            lastPlant = plant;
+        } 
+        else 
+        {
+            lastPlant = null;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.layer != LayerMask.NameToLayer("Plants")) return;
+
+        // rotate can clockwise
+        rb.SetRotation(rotationAngle);
+
+        fallingFertilizer.gameObject.SetActive(true);
+
+        // TODO: send event that watering is happening
+        // Color currentColor = soil.GetComponent<SpriteRenderer>().color;
+        // soil.GetComponent<SpriteRenderer>().color = Color.Lerp(currentColor, Color.black, Mathf.PingPong(Time.time, 1));
+
+        soundManager.PlayFertilizerSound();
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.layer != LayerMask.NameToLayer("Plants")) return;
+
+        // rotate can to default angle
+        rb.SetRotation(0);
+
+        //fallingWater.transform.rotation = originalRotation;
+        fallingFertilizer.gameObject.SetActive(false);
+        soundManager.StopFertilizerSound();
+    }
+}
